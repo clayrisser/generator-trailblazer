513b8f03cbf64b6e56d513ffed25e32d8edaae78
Jam Risser <jam@jamrizzi.com>
2017-02-24 02:39:28 -0600

    Initial commit

---
+++ config/main.js
@@ -0,0 +1,34 @@
+/**
+ * Trailpack Configuration
+ * (app.config.main)
+ *
+ * @see http://trailsjs.io/doc/config/main
+ */
+
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+
+  /**
+   * Order does *not* matter. Each module is loaded according to its own
+   * requirements.
+   */
+  packs: [
+    require('trailpack-repl'),
+    require('trailpack-router'),
+    require('trailpack-express'),
+    require('trailpack-waterline'),
+    require('trailpack-footprints'),
+    require('trailpack-swagger')
+  ],
+
+  /**
+   * Define application paths here. "root" is the only required path.
+   */
+  paths: {
+    root: path.resolve(__dirname, '..'),
+    temp: path.resolve(__dirname, '..', '.tmp')
+  }
+};
---
+++ config/swagger.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+
+  "paths": {
+    "/default/info": {
+      "get": {
+        "description": "Return some info about this application"
+      }
+    }
+  }
+};
