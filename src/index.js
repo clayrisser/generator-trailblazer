02ca569701409e5019c0c40fa89f485a4ca30fc3
Jam Risser <jam@jamrizzi.com>
2017-09-15 20:01:45 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-export default 'Welcome to generator-trailblazer'
+import Generator from 'yeoman-generator';
+
+export default class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.option('babel');
+  }
+
+  method1() {
+    this.log('method ha just ran');
+  }
+
+  method2() {
+    this.log('method 2 just ran');
+  }
+};
